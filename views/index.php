dbc39643695f9405c53933ee75a6f5bc5d5a90a4
Martin Civáň <martin.civan5@gmail.com>
2023-04-04 21:31:08 +0200

    JS filter

--- views/index.php
+++ views/index.php
@@ -1,6 +1,24 @@
 <h1>PHP Test Application</h1>
 
-<table>
+<script>
+    function filterByCity(event) {
+        let table = document.getElementById('users-table-body');
+        let tr = table.getElementsByTagName('tr');
+        for(let row of tr) {
+            let tds = [...row.getElementsByTagName('td')];
+            let values = tds.filter(td => td.classList.contains('city-value'))
+            let city = values[0].innerText;
+            if(city.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1) {
+                row.style.display = '';
+            } else {
+                row.style.display = 'none';
+            }
+        }
+    }
+</script>
+
+<input type="search" placeholder="Search City" onkeyup="filterByCity(event)">
+<table class="table table-striped">
 	<thead>
 		<tr>
 			<th>Name</th>
@@ -8,12 +26,12 @@
 			<th>City</th>
 		</tr>
 	</thead>
-	<tbody>
+	<tbody id="users-table-body">
 		<?foreach($users as $user){?>
 		<tr>
-			<td><?=$user->getName()?></td>
-			<td><?=$user->getEmail()?></td>
-			<td><?=$user->getCity()?></td>
+			<td class="name-value"><?=$user->getName()?></td>
+			<td class="email-value"><?=$user->getEmail()?></td>
+			<td class="city-value"><?=$user->getCity()?></td>
 		</tr>
 		<?}?>
 	</tbody>
